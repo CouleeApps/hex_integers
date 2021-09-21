3e592a2d26ef8513839317dcdabd388ab24ffb38
Glenn Smith <couleeapps@gmail.com>
2021-09-21 00:03:45 -0400

    Unpack top-level generators and shorthand ints in containers
    Now you can `bv.functions` again (after 2969)

--- __init__.py
+++ __init__.py
@@ -1,18 +1,26 @@
-from collections.abc import Iterable, Mapping
+from collections.abc import Generator, Iterable, Mapping
+import binaryninja
 import sys
 
 original_displayhook = sys.__displayhook__
 
-def convert_to_hexint(value, seen):
+
+def convert_to_hexint(value, seen, top=False):
 	if value in seen:
 		return "<recursion>"
 	seen = seen + [value]
 
 	if isinstance(value, (int,)):
-		return f"{value} / 0x{value:x}"
+		if top:
+			return f"{value} / 0x{value:x}"
+		else:
+			return f"0x{value:x}"
 	elif isinstance(value, (float,)) and (value % 1) < 0.0001:
 		value = int(value)
-		return f"~{value} / ~0x{value:x}"
+		if top:
+			return f"~{value} / ~0x{value:x}"
+		else:
+			return f"~0x{value:x}"
 	elif isinstance(value, (str,)):
 		return repr(value)
 	elif isinstance(value, (tuple,)):
@@ -23,6 +31,8 @@ def convert_to_hexint(value, seen):
 		return '{' + ', '.join(convert_to_hexint(k, seen) + ': ' + convert_to_hexint(v, seen) for k,v in value.items()) + '}'
 	elif isinstance(value, (set,)):
 		return '{' + ', '.join(convert_to_hexint(v, seen) for v in value) + '}'
+	elif value is Ellipsis:
+		return '...'
 	#elif isinstance(value, (Iterable,)):
 	#	return '(' + ', '.join(convert_to_hexint(v, seen) for v in value) + ')'
 	#elif isinstance(value, (Mapping,)):
@@ -32,9 +42,15 @@ def convert_to_hexint(value, seen):
 
 
 def new_displayhook(value):
-	seen = []
-	print(convert_to_hexint(value, seen))
-
+	if isinstance(value, (Generator,)) or hasattr(value, '__next__'):
+		conts = []
+		for v in value:
+			conts.append(v)
+			if len(conts) > 100:
+				conts.append(Ellipsis)
+				break
+		value = conts
+	print(convert_to_hexint(value, [], True))
 
 
 setattr(sys, 'displayhook', new_displayhook)
