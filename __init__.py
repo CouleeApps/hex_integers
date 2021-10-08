4443733eef731c2856aa3fcc28153e0c8a37832a
Glenn Smith <couleeapps@gmail.com>
2021-10-08 15:03:31 -0400

    Actually, enums already include the decimal form in repr()

--- __init__.py
+++ __init__.py
@@ -11,10 +11,14 @@ def convert_to_hexint(value, seen, top=False):
 	seen = seen + [value]
 
 	if isinstance(value, (int,)):
-		# Could be an enum
-		if value.__repr__ != (0).__repr__:
+		# Could be an enum or something else with a custom repr()
+		if value.__repr__.__qualname__ != 'int.__repr__':
 			if top:
-				return f"{repr(value)} / {value} / 0x{value:x}"
+				# Enums already include the decimal in repr()
+				if f"{value}" in repr(value):
+					return f"{repr(value)} / 0x{value:x}"
+				else:
+					return f"{repr(value)} / {value} / 0x{value:x}"
 			else:
 				return repr(value)
 		else:
@@ -40,10 +44,6 @@ def convert_to_hexint(value, seen, top=False):
 		return '{' + ', '.join(convert_to_hexint(v, seen) for v in value) + '}'
 	elif value is Ellipsis:
 		return '...'
-	#elif isinstance(value, (Iterable,)):
-	#	return '(' + ', '.join(convert_to_hexint(v, seen) for v in value) + ')'
-	#elif isinstance(value, (Mapping,)):
-	#	return '{' + ', '.join(convert_to_hexint(k, seen) + ': ' + convert_to_hexint(value[k], seen) for k in value) + '}'
 	else:
 		return repr(value)
 
